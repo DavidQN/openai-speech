c3f0fa87c11cd9ce39083b15a342007d006525da
david <david@davids-MacBook-Pro.local>
2023-07-24 22:49:06 -0400

    late night typos resolved...SMH

--- regular-langchain/app.py
+++ regular-langchain/app.py
@@ -1,8 +1,9 @@
-from langchain.agent import Tool
-from langchain.memory import CoversationBufferMemory
+from langchain.agents import Tool
+from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
-from langchain.utilites import GoogleSearchAPIWrapper
-from langchain.agents import initalize_agent
+from langchain.utilities import GoogleSearchAPIWrapper
+
+from langchain.agents import initialize_agent
 
 
 search = GoogleSearchAPIWrapper()
@@ -11,12 +12,12 @@ tools = [
          name = "Search",
          func=search.run,
          description="useful for when you need to answer questions about current events or the current"
-    ),
+    )
 ]
 
-memory = ConversationBufferMemory(memory_key="chat_histroy", return_messages=True)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 llm=ChatOpenAI(temperature=0)
-agent_chain = initalize_agent(tools, llm, agent="chat-conversational-react-description",verbose=True, memory=memory)
+agent_chain = initialize_agent(tools, llm, agent="chat-conversational-react-description",verbose=True, memory=memory)
 
 agent_chain.run(input="what is the weather currently in new york city?")
